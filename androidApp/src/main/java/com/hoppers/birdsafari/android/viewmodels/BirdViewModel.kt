cee29c314b22b584f78d6704e20122cec98fe805
Jitendra Prajapati <jk2praj@gmail.com>
2023-12-11 10:34:16 +0800

    Jk2pr patch 1 (#6)

--- androidApp/src/main/java/com/hoppers/birdsafari/android/viewmodels/BirdViewModel.kt
+++ androidApp/src/main/java/com/hoppers/birdsafari/android/viewmodels/BirdViewModel.kt
@@ -2,19 +2,26 @@ package com.hoppers.birdsafari.android.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hoppers.birdsafari.android.model.BirdsUiState
+import com.hoppers.birdsafari.android.model.UIState
+import com.hoppers.birdsafari.android.model.BirdCategoryState
 import com.hoppers.birdsafari.repositories.BirdRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class BirdViewModel(private val repository: BirdRepository) : ViewModel() {
-    private val _uiState = MutableStateFlow(BirdsUiState())
-    val uiState = _uiState.asStateFlow()
+    private val _categoryState = MutableStateFlow(BirdCategoryState())
+    private val _uiState = MutableStateFlow<UIState>(UIState.Loading)
+    val categoryState = _categoryState.asStateFlow()
+    val birdState = _uiState.asStateFlow()
 
     init {
-        updateImages()
+        getImages()
     }
 
     override fun onCleared() {
@@ -22,16 +29,26 @@ class BirdViewModel(private val repository: BirdRepository) : ViewModel() {
     }
 
     fun selectCategory(category: String) {
-        _uiState.update {
+        _categoryState.update {
             it.copy(selectedCategory = category)
         }
     }
 
-    private fun updateImages() {
-        viewModelScope.launch {
-            val images = repository.getImages()
-            _uiState.update {
-                it.copy(images = images)
+    private fun getImages() {
+        viewModelScope.launch(Dispatchers.Main) {
+            flow<UIState> {
+                val images = repository.getImages()
+                emit(
+                    UIState.UI(
+                        run {
+                            _categoryState.update { it.copy(images = images) }
+                            _categoryState.value
+                        })
+                )
+            }.catch {
+                //emit(BirdState.Error(it.message ?: "Error -----"))
+            }.flowOn(Dispatchers.IO).collect { state ->
+                _uiState.value = state
             }
         }
     }
